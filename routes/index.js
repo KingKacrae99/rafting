0468b8fc24736a69fa68da4c582678d00ef3210d
KingKacrae99 <kacrae9@gmail.com>
2025-09-25 04:22:59 +0100

    completed

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const utils = require('../utilities/index')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger/swagger.json');
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.use('/api', swaggerUi.serve);
+router.get('/api', swaggerUi.setup(swaggerDocument));
 
 router.get('/', (req,res)=>{
     res.send("hello clown (: ")
